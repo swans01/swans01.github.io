16122bb850959bc916a4eb4b08873d7aebbd17f3
swandy <swandy.raja@gmail.com>
2017-10-28 16:15:50 +0700

    fix bug on wikipediaViewer

--- WikipediaViewer/javascript.js
+++ WikipediaViewer/javascript.js
@@ -1,19 +1,18 @@
 
 var api = "https://en.wikipedia.org/w/api.php?action=opensearch&format=json&callback=?&search=";
 $( document ).ready(function() {
-var input = $("#input");
-input.change(wiki);
-
-function wiki() {
+$("input").change(function(){
 	var search = document.getElementById('input').value;
 	$.getJSON(api+search, function(json){
+		$("#div1").empty();
 		for (var i = 0; i < 10; i++) {
 			var title = "<h3><a target='blank' href="+json[3][i]+">"+json[1][i]+"<a/></h3>";
 			var text = "<p>"+json[2][i]+"</p>";
     		$("#div1").append(title, text);     
 		}
 	});
-}
+
+});
 });
 
 
